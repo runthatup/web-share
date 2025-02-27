ab9f7810ab648e0f12d242d6ec7fdacbc4b11314
pon <pon@nfiny.com>
2025-02-27 18:32:27 +0700

    updates: api url

--- src/config/config.base.ts
+++ src/config/config.base.ts
@@ -1,5 +1,5 @@
 export const BaseConfig = {
   appStoreUrl: "https://apps.apple.com/app/runthatup/id6477720228",
-  // apiUrl: "https://api.runthatup.com",
-  apiUrl: "http://localhost:9000",
+  apiUrl: "https://api.runthatup.com",
+  // apiUrl: "http://localhost:9000",
 }
